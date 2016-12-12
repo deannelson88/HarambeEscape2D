ab721f2d279ad5457100b09ce28b714fc7a36e50
JonathanCoey <jonathan.coey@yahoo.co.uk>
2016-12-12 09:14:15 +0000

    Zoo Finish w/ Title Screen + Pause Menu

---
+++ Assets/_Scripts/Level Scripts/LevelLoader.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    private bool playerInZone;
+
+    public string levelToLoad;
+
+    //
+
+    void Start()
+    {
+        playerInZone = false;
+    }
+
+    //
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && playerInZone)
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+
+    //
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.name == "Player")
+        {
+            playerInZone = true;
+        }
+    }
+
+    //
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            playerInZone = false;
+        }
+    }
+
+}
---
+++ Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject currentCheckpoint;
+    private Player player;
+
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+
+
+    public void RespawnPlayer()
+    {
+        Debug.Log("Player Respawn");
+        player.transform.position = currentCheckpoint.transform.position;
+    }
+
+}
